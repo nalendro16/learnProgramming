41cb4faa76d6e06af3906509fa0004934b39de77
Nalendro Agung Wicaksono <66253342+nalendro16@users.noreply.github.com>
2020-12-05 22:26:12 +0700

    Learn controlflow on JS

---
+++ Javascript-Path/3. controlFlow/penkondisian.js
@@ -0,0 +1,7 @@
+var angka = prompt('Masukan Nomor: ')
+
+if( angka%2 == 0 ){
+    alert(angka + 'adalah GENAP')
+}else{
+    alert(angka + 'adalah GANJIL')
+}
---
+++ Javascript-Path/3. controlFlow/script.js
@@ -0,0 +1,5 @@
+alert('Hello')
+for(var i=0; i<5; i++){
+    alert('Hello Agung')
+}
+alert('Thanks')
