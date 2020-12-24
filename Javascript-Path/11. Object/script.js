1bab6987220235c7ddfa9d23ac1c8cbe943e7833
nalendro16 <nalendro_agung@yahoo.com>
2020-12-24 18:51:19 +0700

    learn javascript Object

---
+++ Javascript-Path/11. Object/script.js
@@ -0,0 +1,14 @@
+let mhs = {
+  nama: "Nalendro Agung",
+  umur: 21,
+  ips: [3.0, 3.0, 3.11, 3.12, 3.12, 3.21],
+  ipk: function () {
+    let total = 0;
+    let ipsem = this.ips;
+    for (i = 0; i < this.ips.length; i++) {
+      total += ipsem[i];
+    }
+    return Math.round(total / ipsem.length);
+  },
+};
+mhs.ipk();
