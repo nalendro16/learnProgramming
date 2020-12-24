e33971fc608051653eb009f6036842591236de00
nalendro16 <nalendro_agung@yahoo.com>
2020-12-24 18:57:57 +0700

    fungsi pembulatan pada object

--- Javascript-Path/11. Object/script.js
+++ Javascript-Path/11. Object/script.js
@@ -1,14 +1,15 @@
 let mhs = {
-  nama: "Nalendro Agung",
+  nama: 'Nalendro Agung',
   umur: 21,
   ips: [3.0, 3.0, 3.11, 3.12, 3.12, 3.21],
   ipk: function () {
-    let total = 0;
-    let ipsem = this.ips;
+    let total = 0
+    let ipsem = this.ips
     for (i = 0; i < this.ips.length; i++) {
-      total += ipsem[i];
+      total += ipsem[i]
     }
-    return Math.round(total / ipsem.length);
+    // fungsi pembulatan
+    return Math.round(total / ipsem.length)
   },
-};
-mhs.ipk();
+}
+mhs.ipk()
