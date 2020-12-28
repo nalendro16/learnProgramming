49ee3060f13f12af3e4f892dfe7ae79a231fa761
nalendro16 <nalendro_agung@yahoo.com>
2020-12-28 12:48:57 +0700

    learn object this on js

---
+++ Javascript-Path/12. Object-This/script.js
@@ -0,0 +1,31 @@
+//THIS
+// var a = 10;
+// console.log(this.a); //sama saja menulis console.log(window.a);
+
+//membuat object
+
+//Cara 1- Function Declaration
+// function halo() {
+//   console.log(this);
+//   console.log("Halo");
+// }
+// this.halo(); //Memanggil function halo
+// This mengembalikan object global
+
+//Cara 2- Object Literal
+// var obj = { a: 10, nama: "agung" };
+// obj.halo = function () {
+//   console.log(this);
+//   console.log("Halo");
+// };
+// obj.halo(); //memanggil obj halo
+//This mengembalikan object yg bersangkutan(halo)
+
+//Cara 3- function construction
+function Halo() {
+  console.log(this);
+  console.log("Halo");
+}
+var obj1 = new Halo();
+var obj2 = new Halo();
+//mengembalikan object yg baru dibuat
