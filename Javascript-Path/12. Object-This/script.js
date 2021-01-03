d7edf4ca7ed56a12aec54452bd15060466a17928
nalendro16 <nalendro_agung@yahoo.com>
2021-01-03 18:59:19 +0700

    Object-this update

--- Javascript-Path/12. Object-This/script.js
+++ Javascript-Path/12. Object-This/script.js
@@ -5,20 +5,20 @@
 //membuat object
 
 //Cara 1- Function Declaration
-function halo() {
-  console.log(this)
-  console.log('Halo')
-}
-this.halo() //Memanggil function halo
+// function halo() {
+//   console.log(this)
+//   console.log('Halo')
+// }
+// this.halo() //Memanggil function halo
 // This mengembalikan object global
 
 //Cara 2- Object Literal
-var obj = { a: 10, nama: 'agung' }
-obj.halo = function () {
-  console.log(this)
-  console.log('Halo')
-}
-obj.halo() //memanggil obj halo
+// var obj = { a: 10, nama: 'agung' }
+// obj.halo = function () {
+//   console.log(this)
+//   console.log('Halo')
+// }
+// obj.halo() //memanggil obj halo
 //This mengembalikan object yg bersangkutan(halo)
 
 //Cara 3- function construction
