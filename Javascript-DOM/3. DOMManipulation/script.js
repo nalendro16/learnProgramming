21b5f0a0aa625e9ee49d58983b94c2db1524e46e
nalendro16 <nalendro_agung@yahoo.com>
2021-05-05 06:40:03 +0700

    DOM manipulation

--- Javascript-DOM/3. DOMManipulation/script.js
+++ Javascript-DOM/3. DOMManipulation/script.js
@@ -1,15 +1,15 @@
-// const judul = document.getElementById("judul");
-// judul.innerHTML = "<i>NALENDRO AGUNG</i>";
+const judul = document.getElementById('judul')
+judul.innerHTML = '<i>NALENDRO AGUNG</i>'
 
-// const el = document.querySelector("section#a");
-// el.innerHTML = "<div>Diganti dgn Js!</div>";
+const el = document.querySelector('section#a')
+el.innerHTML = '<div>Diganti dgn Js!</div>'
 
-const p = document.getElementsByTagName('p')[3]
-p.style.backgroundColor = 'blue'
+// const p = document.getElementsByTagName('p')[3]
+// p.style.backgroundColor = 'blue'
 
-const judul = document.getElementsByTagName('h1')[0]
-const a = document.querySelector('section#a a')
-a.setAttribute('id', 'link')
+// const judul = document.getElementsByTagName('h1')[0]
+// const a = document.querySelector('section#a a')
+// a.setAttribute('id', 'link')
 
 // const pp = document.getElementsByTagName("p");
 // pp[1].classList.add("label");
