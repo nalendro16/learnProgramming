c86f33c3afa1215467dc0462876d1afefbf00dcc
nalendro16 <nalendro_agung@yahoo.com>
2021-05-05 06:39:11 +0700

    DOM manipulation

--- Javascript-DOM/3. DOMManipulation/script.js
+++ Javascript-DOM/3. DOMManipulation/script.js
@@ -4,8 +4,8 @@
 // const el = document.querySelector("section#a");
 // el.innerHTML = "<div>Diganti dgn Js!</div>";
 
-// const p = document.getElementsByTagName("p")[3];
-// p.style.backgroundColor = "blue";
+const p = document.getElementsByTagName('p')[3]
+p.style.backgroundColor = 'blue'
 
 const judul = document.getElementsByTagName('h1')[0]
 const a = document.querySelector('section#a a')
