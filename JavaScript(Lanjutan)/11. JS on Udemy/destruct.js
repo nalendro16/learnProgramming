6de1a3afa1b7e3c6513f6b5cbf1a88b37727eb55
nalendro16 <nalendro_agung@yahoo.com>
2021-06-25 23:13:18 +0700

    destruct array and object

--- JavaScript(Lanjutan)/11. JS on Udemy/destruct.js
+++ JavaScript(Lanjutan)/11. JS on Udemy/destruct.js
@@ -1,3 +1,9 @@
-const name = ['nalendro', 'agung', 'wicaksono', 'nal']
-;[nm1, , , nm4] = name
-console.log(nm1, nm4)
+// destruct array
+// const name = ['nalendro', 'agung', 'wicaksono', 'nal']
+// ;[nm1, , , nm4] = name
+// console.log(nm1, nm4)
+
+// destruct object
+const bio = { nama: 'nalendro', usia: 22, gender: 'laki-laki' },
+  { nama, usia } = bio
+console.log(nama, usia)
